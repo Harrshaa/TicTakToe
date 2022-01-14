7f0e2f6a14eb481eb781194b227af187a2743889
Harrshaa <sriharsha.mutakodoor@gmail.com>
2022-01-14 23:11:10 +0530

    board.js and square.js

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Square = ({value}) => {
+    
+    return (
+            <button type="button">{value}</button>  //what if instead of 1 f we have dynamic value//
+    )
+};
+
+export default Square
